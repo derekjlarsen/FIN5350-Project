c5a51ef810840de4a41de889fa70b4082daa6381
stephenbreeze <42820124+stephenbreeze@users.noreply.github.com>
2018-12-10 16:35:02 -0700

    Add files via upload

---
+++ test_american.py
@@ -0,0 +1,30 @@
+from probo.marketdata import MarketData
+from probo.payoff import VanillaPayoff, call_payoff, put_payoff
+from probo.engine import BinomialPricingEngine, AmericanBinomialPricer 
+from probo.facade import OptionFacade
+import numpy as np
+
+## Set up the market data
+
+
+thedata = MarketData()
+thedata.historic('ZION', 20181011)
+expiry = 1.0 
+strike = 40.0
+thecall = VanillaPayoff(expiry, strike, call_payoff)
+theput = VanillaPayoff(expiry, strike, put_payoff)
+
+## Set up the European Binomial pricer
+steps = 3 
+pricer = AmericanBinomialPricer
+binomengine = BinomialPricingEngine(steps, pricer) 
+
+## Calculate the price
+option1 = OptionFacade(thecall, binomengine, thedata)
+price1 = option1.price()
+print("The call price via American Binomial is: {0:.3f}".format(price1))
+option2 = OptionFacade(theput, binomengine, thedata)
+price2 = option2.price()
+print("The put price via American Binomial is: {0:.3f}".format(price2))
+
+
